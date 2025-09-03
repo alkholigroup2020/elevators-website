956b3034ce99a178029a5c97453ec437acdfaf75
Fawzy Mohamed <fawzy.mohamed@live.com>
2025-09-03 07:31:31 +0300

    update google tag

--- src/lib/Analytics.svelte
+++ src/lib/Analytics.svelte
@@ -1,25 +1,17 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-
-	$: {
-		if (typeof gtag !== 'undefined') {
-			gtag('config', 'G-4F881GX5V7', {
-				page_title: document.title,
-				page_path: $page.url.pathname
-			});
-		}
-	}
-</script>
-
 <svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-4F881GX5V7"></script>
+	<!-- Google Tag Manager -->
+	<!-- Google Tag Manager -->
 	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-
-		gtag('config', 'G-4F881GX5V7');
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-NZCJJDX');
 	</script>
+	<!-- End Google Tag Manager -->
 </svelte:head>
